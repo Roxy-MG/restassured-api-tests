bb052c86eaf2c36297f6d859d7fccb9bc89c2318
郭世鑫 <guo.shixin@darcytech.com>
2026-04-18 20:48:50 +0800

    feat: 补充超时时间

--- src/test/java/config/TestConfig.java
+++ src/test/java/config/TestConfig.java
@@ -3,6 +3,8 @@ package config;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.config.HttpClientConfig;
+import io.restassured.config.RestAssuredConfig;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
@@ -22,12 +24,22 @@ public class TestConfig {
     private static LocalDateTime tokenExpireTime;
     public static final int TOKEN_VALID_TIME = 55;
 
-    // 请求配置
+    // 请求配置，默认连接超时时间5秒，读取超时时间10秒
     public static RequestSpecification getRequestSpec() {
+        return getRequestSpec(5000, 10000);
+    }
+
+    // 请求配置，自定义超时时间
+    public static RequestSpecification getRequestSpec(int connectionTime, int socketTime) {
         return new RequestSpecBuilder()
                 .setBaseUri(BASE_URL)
                 .setContentType(ContentType.JSON)
                 .addHeaders(X_API_KEY)
+                .setConfig(RestAssuredConfig.config().httpClient(
+                        HttpClientConfig.httpClientConfig()
+                                .setParam("http.connection.timeout", connectionTime)
+                                .setParam("http.socket.timeout", socketTime)
+                ))
                 .log(LogDetail.BODY)
                 .addFilter(new AllureRestAssured())
                 .build();
